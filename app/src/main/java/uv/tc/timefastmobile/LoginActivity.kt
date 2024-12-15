2863134ca609a1158d63e7599f95fcf761aefd35
xFractu <marioteku99@gmail.com>
2024-12-15 17:14:48 -0600

    Login Implementado

--- app/src/main/java/uv/tc/timefastmobile/LoginActivity.kt
+++ app/src/main/java/uv/tc/timefastmobile/LoginActivity.kt
@@ -6,38 +6,86 @@ import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.view.MotionEvent
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
+import com.koushikdutta.ion.Ion
 import uv.tc.timefastmobile.databinding.ActivityLoginBinding
+import uv.tc.timefastmobile.poko.LoginColaborador
+import uv.tc.timefastmobile.util.Constantes
 import kotlin.reflect.KMutableProperty0
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private var passwordActual = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-
-        setupPasswordVisibilityToggle(binding.etPasswordLogin, ::passwordActual)
-
+        setupPasswordVisibilityToggle(binding.etPassword, ::passwordActual)
 
         binding.btnLoginIngresar.setOnClickListener {
-            irPantallaInicio()
+            val noPersonal = binding.etNoPersonal.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (sonCamposValidos(noPersonal, password)) {
+                verificarCredenciales(noPersonal, password)
+            }
         }
-
     }
-    fun irPantallaInicio(){
 
-        val intent = Intent(this@LoginActivity,InicioActivity::class.java)
+    private fun sonCamposValidos(noPersonal: String, password: String): Boolean {
+        var camposValidos = true
+
+        if (noPersonal.isEmpty()) {
+            camposValidos = false
+            binding.etNoPersonal.error = "Número de personal obligatorio"
+        }
+        if (password.isEmpty()) {
+            camposValidos = false
+            binding.etPassword.error = "Contraseña obligatoria"
+        }
+        return camposValidos
+    }
+
+    private fun verificarCredenciales(noPersonal: String, password: String) {
+        // Configuración de Ion (solo la primera vez)
+        Ion.getDefault(this@LoginActivity).conscryptMiddleware.enable(false)
+
+        // Llamada al Web Service
+        Ion.with(this@LoginActivity)
+            .load("POST", "${Constantes().URL_WS}/conductores/iniciarSesion")
+            .setHeader("Content-Type", "application/x-www-form-urlencoded")
+            .setBodyParameter("noPersonal", noPersonal)
+            .setBodyParameter("password", password)
+            .asString()
+            .setCallback { e, result ->
+                if (e == null) {
+                    procesarRespuesta(result)
+                } else {
+                    Toast.makeText(this@LoginActivity, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun procesarRespuesta(json: String) {
+        val gson = Gson()
+        val respuesta = gson.fromJson(json, LoginColaborador::class.java)
+        Toast.makeText(this@LoginActivity, respuesta.mensaje, Toast.LENGTH_LONG).show()
+
+        if (!respuesta.error) {
+            val colaboradorJSON = gson.toJson(respuesta.colaborador)
+            irPantallaInicio(colaboradorJSON)
+        }
+    }
+
+    private fun irPantallaInicio(colaborador: String) {
+        val intent = Intent(this@LoginActivity, InicioActivity::class.java)
+        intent.putExtra("colaborador", colaborador)
         startActivity(intent)
         finish()
-
-
     }
 
     private fun setupPasswordVisibilityToggle(editText: EditText, visibilityState: KMutableProperty0<Boolean>) {
--- app/src/main/java/uv/tc/timefastmobile/util/Constantes.kt
+++ app/src/main/java/uv/tc/timefastmobile/util/Constantes.kt
@@ -1,5 +1,6 @@
 package uv.tc.timefastmobile.util
 
 class Constantes {
-    val URL_WS = "http://192.168.100.44/time-fast-api/api/"
+
+    val URL_WS = "http://192.168.100.84:8084/time-fast-api/api/"
 }
